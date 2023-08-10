ca4b01e7cac01269474a6f9fbbc946137208070f
sara-elghobary <soorfour@gmail.com>
2023-08-10 15:46:19 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Entry point of the program
  *
- * Return: 1 (Error)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, msg, 59);
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, STDERR_FILENO, msg, 59);
 
-    return (1);
+	return (1);
 }
