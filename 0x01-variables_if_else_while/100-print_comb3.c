7167ec6967da00049dcd54248415096897f1673a
sara-elghobary <soorfour@gmail.com>
2023-08-12 08:11:31 -0700

    d

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,26 +11,25 @@
  */
 int main(void)
 {
-    int first_digit;
-    int second_digit;
+int first_digit;
+int second_digit;
 
-    for (first_digit = 0; first_digit < 9; first_digit++)
-    {
-        for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
-        {
-            putchar(first_digit + '0');
-            putchar(second_digit + '0');
+for (first_digit = 0; first_digit < 9; first_digit++)
+{
+for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+{
+putchar(first_digit + '0');
+putchar(second_digit + '0');
 
-            if (first_digit != 8 || second_digit != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+if (first_digit != 8 || second_digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 
+putchar('\n');
+
+return (0);
+}
