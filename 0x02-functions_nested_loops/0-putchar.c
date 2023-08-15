d1577194586fcb5b3da3c4d61cbea0153c997955
sara-elghobary <soorfour@gmail.com>
2023-08-15 08:30:10 -0700

    h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#imclude "main.h"
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int ch;
+
+	for (ch = 0 ; ch <= 7 ; ch++)
+	{
+		_putchar(c[ch]);
+	}
+	_putchar("\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifnder MYHEADERS_H
+#define MYHEADERS_H
+
+int add(int a, intb);
+
+#endif
