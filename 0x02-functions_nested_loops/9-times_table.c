777f1288afc0e5bc30398e7d1c2b5273bb0005f3
sara-elghobary <soorfour@gmail.com>
2023-08-16 07:22:47 -0700

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,24 +4,31 @@
 /**
  * times_table - Prints the 9 times table
  *
- * Description: This function prints the 9 times table in the specified format.
-*/
+ * Description: This function prints the 9 times table with specified format.
+ */
 void times_table(void)
 {
 int i, j, p;
+
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 p = i * j;
-           
+
 if (j > 0)
 {
 _putchar(',');
 _putchar(' ');
+
+if (p < 10)
+_putchar(' ');
 }
-_putchar(p + '0');
-}        
+
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+}
+
 _putchar('\n');
 }
 }
