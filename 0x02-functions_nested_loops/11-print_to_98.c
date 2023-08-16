2ebaf520ef6729c72f09bd22352e73234f18fbf8
sara-elghobary <soorfour@gmail.com>
2023-08-16 08:59:28 -0700

    h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,40 +5,41 @@
  * print_to_98 - Prints all numbers from n to 98
  * @n: The starting number
  *
- * Description: This function prints all numbers from n to 98, separated by commas and spaces.
+ * Description: This function prints
+ * all numbers from n to 98, separated by commas and spaces.
  */
 void print_to_98(int n)
 {
-    int i;
+int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i < 98)
-            {
-                printf("%d, ", i);
-            }
-            else
-            {
-                printf("%d", i);
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            if (i > 98)
-            {
-                printf("%d, ", i);
-            }
-            else
-            {
-                printf("%d", i);
-            }
-        }
-    }
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i < 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+if (i > 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
+}
+}
 
-    printf("\n");
+printf("\n");
 }
