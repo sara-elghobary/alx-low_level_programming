d855daeeda1c8b2ec9faf56db1efe2229e771fb6
sara-elghobary <soorfour@gmail.com>
2023-08-16 02:28:58 -0700

    h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
 
+/**
+ * _isalpha - Checks if a character is alphabetic
+ *
+ * *@c: The character to check
+ *
+ * *Return: 1 if the character is alphabetic, 0 otherwise
+ *
+ * @description This function checks if a character is alphabetic.
+ */
 int _isalpha(int c)
 {
 return (isalpha(c));
