f925703136918ae1f3f1cf316b62db925cd729f8
sara-elghobary <soorfour@gmail.com>
2023-08-17 14:57:36 -0700

    h

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,11 @@
  */
 void print_most_numbers(void)
 {
-for (int i = 0 ; i <= 9 ; i++)
+int i;
+
+for (i = 0 ; i <= 9 ; i++)
 {
-if (i != 2 && i != 4)
+if ((i != 2) && (i != 4))
 {
 _putchar(i + '0');
 }
