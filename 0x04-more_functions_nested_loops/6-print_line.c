83de470c4adb1bb94bf7e7e8854b271bf04ae6a3
sara-elghobary <soorfour@gmail.com>
2023-08-17 16:03:40 -0700

    h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1 ; i <= n ; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
