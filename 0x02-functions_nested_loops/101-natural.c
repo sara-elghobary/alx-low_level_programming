e9ff7bdf60db022d4bb3286d8d282906197c3d21
sara-elghobary <soorfour@gmail.com>
2023-08-16 17:55:22 -0700

    h

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
- * Description: This program calculates the sum of divisors of 1024.
- *
- * Return: Always 0 (Success)
- */
-int main() {
-    int n = 1;
-    int sum = 0;
-
-    while (n <= 1024)
-    {
-        if (1024 % n == 0)
-        {
-            sum += n;
-        }
-        n++;
-    }
-    printf("%d", sum);
-    return sum;
+*main - prints count of multiples
+*of 3 or 5 below 1024
+*Return: return 0
+*/
+int main(void)
+{
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+sum += n;
+}
+printf("%d\n", sum);
+return (0);
 }
