34a5653e72811519c7d9c7cab14e8ff23c64843b
sara-elghobary <soorfour@gmail.com>
2023-08-18 12:54:05 -0700

    h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 1;
+
+while (i <= 100)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+if (i == 100)
+printf("Buzz");
+else
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+i++;
+}
+
+printf("\n");
+return (0);
+}
