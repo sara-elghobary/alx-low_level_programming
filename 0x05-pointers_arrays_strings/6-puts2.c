5ce157edca44dd27211f74c66af5df2bfd52e67a
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-22 00:14:56 +0300

    h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The input string.
+ */
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 != 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
