03460cdd97b86a2bf1463ea4e2a3216e0ff0f7ad
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-21 22:31:16 +0300

    h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,6 +4,23 @@
  * print_rev - Prints a string in reverse, followed by a newline.
  * @s: The string to be printed in reverse.
  */
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
 void print_rev(char *s)
 {
 int i;
