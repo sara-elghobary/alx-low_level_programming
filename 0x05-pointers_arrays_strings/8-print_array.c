4abd383097bbcc68e5427c3acefd00ad1fc47c1b
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-22 12:36:01 +0300

    h

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Prints an array of integers.
+ * @a: The array to be printed.
+ * @n: The number of elements in the array.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+
+printf("\n");
+}
