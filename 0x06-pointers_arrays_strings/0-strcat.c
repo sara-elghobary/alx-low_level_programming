faa03950a9c223257820974d18ee29ce1e9f2095
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-23 20:20:53 +0300

    h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the concatenated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+int len = _strlen(dest);
+int len2 = _strlen(src);
+char stir[(len + len2) + 1];
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+stir[i] = dest[i];
+}
+
+for (j = 0; src[j] != '\0'; j++, i++)
+{
+stir[i] = src[j];
+}
+
+stir[i] = '\0';
+src = stir;
+return (src);
+}
