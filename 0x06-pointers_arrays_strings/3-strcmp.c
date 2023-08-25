c97cde38d9c796fdc2da3af8671ae2324d90c7b6
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-25 19:57:27 +0300

    h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,29 +2,17 @@
 
 /**
  * _strcmp - Compares two strings.
- * @s1: The first string.
- * @s2: The second string.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
  *
- * Return: Negative value if s1 is less than s2,
- *         Positive value if s1 is greater than s2,
- *         0 if both strings are equal.
+ * Return: An integer less than, equal to, or greater than zero
+ *         if s1 is found, respectively, to be less than, to match,
+ *         or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0')
+while (*s1 != '\0' && *s1 == *s2)
 {
-if (*s1 == *s2)
-return (0);
-else if (*s1 > *s2)
-{
-return (*s1 - *s2);
-break;
-}
-else if (*s1 < *s2)
-{
-return (*s1 - *s2);
-break;
-}
-*s1++;
-}
+s1++;
+s2++;
 }
