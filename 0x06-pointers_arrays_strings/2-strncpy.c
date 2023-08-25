472d5667460a7b62485fd716c5059116f3cfe5e4
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-25 17:40:32 +0300

    h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
+ * _strncpy - Copies a string up to n bytes.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
