1e5f8f9539c5632d4b1acb5c43b30faed7785f23
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-25 16:06:27 +0300

    h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,23 @@
 #include “main.h”
 
 /**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
  * _strcat - Concatenates two strings.
  * @dest: The destination string.
  * @src: The source string.
