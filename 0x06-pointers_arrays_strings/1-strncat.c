f1db09cb2004f4fcf5a68fb07a33a6d8fc9be4cb
root <root@DESKTOP-MLOUBAH.localdomain>
2023-08-25 17:10:51 +0300

    h

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to concatenate from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int len = _strlen(dest);
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[len] = src[i];
+len++;
+}
+dest[len] = '\0';
+return (dest);
+}
