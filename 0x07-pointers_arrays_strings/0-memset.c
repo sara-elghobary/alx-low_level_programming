890c00c968d007a3735a2852ae0ca01281e0e781
sara-elghobary <soorfour@gmail.com>
2023-08-29 12:59:26 +0300

    h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,7 +12,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i;
+unsigned int i;
 
 if (n > 0)
 {
@@ -22,41 +22,4 @@ s[i] = b;
 }
 }
 return (s);
-}
-
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
 }
