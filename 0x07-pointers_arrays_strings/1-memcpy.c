a225328986734a44293a83e6d77ec245cbfd943f
sara-elghobary <soorfour@gmail.com>
2023-08-29 14:00:16 +0300

    h

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+/**
+ * _memcpy - Copy n bytes from src to dest
+ * @dest: Destination memory buffer
+ * @src: Source memory buffer
+ * @n: Number of bytes to copy
+ * Return: Pointer to destination memory buffer
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i ;
+unsigned int i;
 
-    if (sizeof(dest) >= sizeof(src))
-    {
-        for(i = 0 ; i < n ; i++)
-        {
-            dest[i] = src[i];
-        }
-        dest[i] = '\0';
-    }
-    return (dest);
+for (i = 0 ; i < n ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
