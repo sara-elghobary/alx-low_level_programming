4aaabc7eba353cdaaf873ac49bfe18ac41b7bffd
sara-elghobary <soorfour@gmail.com>
2023-09-01 01:26:58 +0300

    h

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,12 +29,12 @@ int is_palindrome_helper(char *s, int start, int end)
 {
 if (start >= end)
 {
-return 1;
+return (1);
 }
 
 if (s[start] != s[end])
 {
-return 0;
+return (0);
 }
 
 return (is_palindrome_helper(s, start + 1, end - 1));
@@ -52,7 +52,7 @@ int length = _strlen(s);
 
 if (length <= 1)
 {
-return 1;
+return (1);
 }
 
 return (is_palindrome_helper(s, 0, length - 1));
