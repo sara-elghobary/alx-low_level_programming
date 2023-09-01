bc878d953dc4f039b1efb3759c3c3e5aafe5d587
sara-elghobary <soorfour@gmail.com>
2023-09-02 01:02:24 +0300

    h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main (int argc ,char *argv[])
-{
-    printf("%s\n",argv[0]);
-    
+int main(int argc, char *argv[]) {
+    // Check if argc is greater than 0 to ensure argv[0] exists.
+    if (argc > 0) {
+        printf("%s\n", argv[0]);  // Print the program name.
+    } else {
+        printf("Program name not available.\n");
+    }
+
+    return 0;
 }
