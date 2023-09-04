ac89c5129a104cc94f282594c2878e8dae7d528e
sara-elghobary <soorfour@gmail.com>
2023-09-04 19:00:23 +0300

    h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
+ * _strcpy - Copies the string pointed to by src, including the null byte,
+ *           to the buffer pointed to by dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
+}
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: The string to duplicate.
+ *
+ * Return: A pointer to the duplicated string, or NULL if str is NULL
+ * or if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
+if (str == NULL)
+return (NULL);
+
+int len = _strlen(str);
+
+char *s;
+s = (char *)malloc(len + 1);
+
+if (s == NULL)
+return (NULL);
+
+_strcpy(s, str);
+
+return (s);
+}
