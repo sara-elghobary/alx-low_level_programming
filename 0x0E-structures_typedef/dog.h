60d9d22f97c9a0a6f995774ea562b3a7b361fbfa
sara-elghobary <soorfour@gmail.com>
2023-09-11 21:34:37 +0300

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,10 @@ char *name;
 float age;
 char *owner;
 }dog_t;
+#endif
+
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
