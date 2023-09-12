20064469704fa8b171cfffb6ff572415a760b82f
sara-elghobary <soorfour@gmail.com>
2023-09-12 17:02:46 +0300

    h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,31 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * new_dog - Creates a new dog with the given name, age, and owner.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: string to copy
  *
- * Return: A pointer to the newly created dog_t struct.
+ * Return: Pointer
+ */
+char *_strdup(char *str)
+{
+	int l, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	l = 0;
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+	{
+		*(s + i) = *(str + i);
+	}
+	return (s);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
 
-dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(struct dog));
 
-if (newDog == NULL)
-return (NULL);
+	if (new_dog == 0 || name == 0 || owner == 0)
+		return (0);
 
-newDog->name = strdup(name);
-newDog->owner = strdup(owner);
-if (newDog->name == NULL || newDog->owner == NULL)
-{
-free(newDog->name);
-free(newDog->owner);
-free(newDog);
-return (NULL);
-}
+	new_dog->name = _strdup(name);
+	if (new_dog->name == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == 0)
+		{
+			free(new_dog);
+			free(new_dog->name);
+			return (0);
+		}
+	return (new_dog);
 }
