d2a34ae556d06deb3e51f4fab2bea470d5820f17
sara-elghobary <soorfour@gmail.com>
2023-09-13 18:01:17 +0300

    h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,4 @@
 #include "function_pointers.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * print_name - Print a name using a specified printing function.
  * @name: Pointer to the name string.
@@ -23,24 +9,7 @@ int _putchar(char c)
  */
 void print_name(char *name, void (*f)(char *))
 {
-int i;
-int len = 0;
 
 f(name);
-
-while (*name != '\0')
-{
-len++;
-name++;
-
-}
-name -= len;
-
-for (i = 0 ; i < len ; i++)
-{
-_putchar(*name);
-name++;
-}
-_putchar('\0');
 
 }
