df419fce29e51db50cffe8e7afd89e2b5569ad56
sara-elghobary <soorfour@gmail.com>
2023-09-14 00:29:25 +0300

    h

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,6 +23,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
+if (array && cmp)
+{
 if (size <= 0)
 return (-1);
 
@@ -30,6 +32,7 @@ for (i = 0 ; i < size ; i++)
 {
 if (cmp(array[i]) != 0)
 return (i);
+}
 }
 return (-1);
 }
