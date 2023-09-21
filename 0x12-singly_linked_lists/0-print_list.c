69c310a72cc84552d6cedbc9faaae4f6de3cb598
sara-elghobary <soorfour@gmail.com>
2023-09-22 01:32:40 +0300

    h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,10 +17,10 @@ while (h != NULL)
 {
 if (h->str == NULL)
 {
-printf("[0] (nil)");
+printf("[0] (nil)\n");
 }
 else
-printf("[%u] (%s)\n", h->len, h->str);
+printf("[%d] (%s)\n", h->len, h->str);
 how_much++;
 h = h->next;
 }
