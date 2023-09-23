c054fb9ca84c7ca73e375838bd2495f329885e68
sara-elghobary <soorfour@gmail.com>
2023-09-23 16:26:34 +0300

    h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before - Prints a specific message to the standard output.
+ *
+ * Description: This function prints the message
+ * "You're beat! and yet, you must allow,"
+ * and "I bore my house upon my back!" to the standard output.
+ * It is used to display a specific message.
+ */
+void before(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
