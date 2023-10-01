cdb54f10fc9719b95193f7f4bd4f36f754e3dccf
sara-elghobary <soorfour@gmail.com>
2023-10-01 21:33:15 +0300

    h

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int len = 0;
-int i;
+unsigned int i;
 listint_t *s;
 listint_t *ss;
 listint_t *new_node;
