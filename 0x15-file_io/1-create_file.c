05a1eb1cdd95ceeda08b3b4c6c72324d7bd8e0c0
sara-elghobary <soorfour@gmail.com>
2023-10-03 15:48:56 +0300

    h

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,31 @@
-#include "main.h"
-#include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
 
 /**
- * create_file - Create a file and write text content to it.
+ * create_file - Create a file with specified content and permissions.
  * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * @text_content: The NULL-terminated string to write
+ * to the file (can be NULL).
  *
- * Return: 1 on success, -1 on failure (file cannot be created,
- * write fails, etc.).
- *         The created file must have permissions: rw-------.
- *         If the file already exists, it will be truncated.
- *         If filename is NULL, return -1.
- *         If text_content is NULL, an empty file will be created.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t written;
 
-if (filename == NULL || text_content == NULL)
-
+if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
 if (fd == -1)
 return (-1);
 
+if (text_content != NULL)
+{
 written = write(fd, text_content, strlen(text_content));
 
 if (written == -1)
@@ -37,6 +33,8 @@ if (written == -1)
 close(fd);
 return (-1);
 }
+}
+
 close(fd);
 return (1);
 }
