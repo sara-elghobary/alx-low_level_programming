29a9b5a479b4340cc27f20119af7861cf2e5abff
sara-elghobary <soorfour@gmail.com>
2023-10-03 16:58:45 +0300

    h

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,36 +1,13 @@
 #include "main.h"
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * sum_them_all - Calculate the sum of a variable number of integers.
+ * main - Entry point of the program
  *
- * This function takes an unsigned integer 'n' and a variable number of integer
- * arguments. It calculates and returns the sum of the first 'n' integer
- * arguments.
- *
- * @n: The number of integers to sum.
- * @ ...: The integer arguments to be summed.
- *
- * Return: The total sum of the first 'n' integer arguments.
+ * Return: Always 0 (Success)
  */
-int sum_them_all(const unsigned int n, ...)
+int main()
 {
-unsigned int i;
-unsigned int total;
-
-va_list sum;
-va_start(sum, n);
-
-if (n == 0)
+printf("Hello\n");
 return (0);
-
-total = 0;
-
-for (i = 0; i < n; i++)
-total += va_arg(sum, int);
-
-
-va_end(sum);
-
-return (total);
 }
