15d9c9fa606139fef6ce233df0f9f098598a5de7
sara-elghobary <soorfour@gmail.com>
2023-11-15 00:06:15 +0200

    h

--- simple_shell/main.h
+++ simple_shell/main.h
@@ -9,21 +9,26 @@
 
 #define MAX_COMMAND_LENGTH 100
 
+extern char **environ;
+
+
+
 
 int _putchar(char c);
-void display_prompt();
+void display_prompt(void);
 void tokenize_command(char *command, char *args[]);
 void execute_command(char *command, int line_number, char *program_name);
-void print_environment_variables();
+void print_environment_variables(void);
 int _strlen(char *s);
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
 char *itoa(int val, int base);
 char *_strcpy(char *dest, char *src);
-void execute_external_command(char *args[], char *program_name, int line_number);
+void exe_external_command(char *args[], char *program_name, int line_number);
 int contains_slash(char *str);
 void handle_invalid_command(char *program_name, int line_number);
-void handle_command_not_found(char *program_name, int line_number, char *command);
+void handle_com_not_found(char *program_name, int line_number, char *command);
+
 
 
 #endif
