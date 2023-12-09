189c65b9a9a068570e3d89c5c70a53980b189556
sara-elghobary <soorfour@gmail.com>
2023-12-09 13:54:00 +0200

    h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all elements of a dlistint_t list
+ * @h: Head of the doubly linked list
+ * Return: Number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+
+    return count;
+}
