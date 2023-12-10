823e8783b4446f2e5cd1208215344d5ba82a2cae
sara-elghobary <soorfour@gmail.com>
2023-12-10 16:11:00 +0200

    h

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert a new node at a given
+ * position in a dlistint_t linked list
+ * @h: Pointer to the address of the head of the doubly linked list
+ * @idx: Index where the new node should be added (starting from 0)
+ * @n: Data to be stored in the new node
+ * Return: The address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h;
+	dlistint_t *new_node;
+	size_t count = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		/* Insert at the beginning of the list */
+		new_node->prev = NULL;
+		new_node->next = current;
+		if (current != NULL)
+			current->prev = new_node;
+		*h = new_node;  /* Update the head pointer */
+		return (new_node);
+	}
+
+	while (current != NULL)
+	{
+		if (count == idx)
+		{
+			/* Insert at the specified index */
+			new_node->prev = current->prev;
+			new_node->next = current;
+			if (current->prev != NULL)
+				current->prev->next = new_node;
+			current->prev = new_node;
+			return (new_node);
+		}
+
+		current = current->next;
+		count++;
+	}
+
+	free(new_node);
+	return (NULL);
+}
